b598bf54b0e8c70bc9ec97ef48cebf57aacd6a86
EC2 Default User <ec2-user@ip-172-31-37-38.us-west-2.compute.internal>
2022-05-13 02:45:09 +0000

    60秒ごとに分がカウントされるよう修正・変数名の変更を行いました。

--- mains.js
+++ mains.js
@@ -3,25 +3,25 @@ const stop = document.getElementById("stop");
 const start= document.getElementById("start");
 const reset = document.getElementById("reset");
 
-let c1 = 0;
-let c2 = 0;
-let c3 = 0;
-let c4 = 0;
+let ms = 0;
+let second = 0;
+let minutes = 0;
+let hour = 0;
 let stopwatch = () => {
-	c1++; //１を足す
-	if (c1 >9) {
-		c2++;
-		c1=0;
+	ms++; 
+	if (ms >9) {
+		second++;
+		ms=0;
 	}
-	if (c2 >9){
-		c3++;
-		c2=0
+	if (second >59){
+		minutes++;
+		second=0
 	}
-	if (c3 >9){
-		c4++;
-		c3=0
+	if (minutes >59){
+		hour++;
+		minutes=0
 	}
-count.innerHTML = c4 + ":" + c3 + ":" + c2 + ":" + c1
+count.innerHTML = hour + ":" + minutes + ":" + second + ":" + ms
 }
 
 let interval;
@@ -41,10 +41,10 @@ stop.addEventListener('click', () => { //ストップ押したときの動作
 
 reset.addEventListener('click', () => { //リセット押したときの動作
 	count.innerHTML = "0:0:0:0";
-	c1 = 0;
-	c2 = 0;
-	c3 = 0;
-	c4 = 0;
+	ms = 0;
+	second = 0;
+	minutes = 0;
+	hour = 0;
 	start.disabled = false;
 	stop.disabled = true;
 	reset.disabled = true;
